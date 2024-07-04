5fc39dedeef37562d23bd0adc2d8c1d24e2f9af8
koen1711 <za4266468@gmail.com>
2024-07-04 16:19:57 +0200

    chore: fix json lib

--- src/dashboard/auth/auth.h
+++
@@ -1,8 +0,0 @@
-#ifndef AUDIORECORDER_AUTH_H
-#define AUDIORECORDER_AUTH_H
-
-#include <uWebSockets/HttpResponse.h>
-
-bool checkAuth(uWS::HttpResponse<true> *res, uWS::HttpRequest *req, bool allowUnregisteredAccess, const std::string& redirect_uri, bool return403 = false);
-
-#endif //AUDIORECORDER_AUTH_H
